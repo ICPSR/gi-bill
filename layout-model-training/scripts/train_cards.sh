4b205c36707b21a063b77a75d0baace5a9aa6958
Sara Katherine Lafia <slafia@gl-login1.arc-ts.umich.edu>
2022-06-24 11:53:23 -0400

    Updating text extraction workflow with custom labels

--- layout-model-training/scripts/train_cards.sh
+++ layout-model-training/scripts/train_cards.sh
@@ -4,10 +4,10 @@ cd /home/slafia/layout-parser/layout-model-training/tools
 
 python train_net.py \
     --dataset_name          card-item \
-    --json_annotation_train ../data/cards-v3/train.json \
-    --image_path_train      ../data/cards-v3/ \
-    --json_annotation_val   ../data/cards-v3/test.json \
-    --image_path_val        ../data/cards-v3/ \
+    --json_annotation_train ../data/cards-v4/train.json \
+    --image_path_train      ../data/cards-v4/ \
+    --json_annotation_val   ../data/cards-v4/test.json \
+    --image_path_val        ../data/cards-v4/ \
     --config-file           ../configs/prima/fast_rcnn_R_50_FPN_3x.yaml \
-    OUTPUT_DIR  ../outputs/cards-v3/fast_rcnn_R_50_FPN_3x/ \
+    OUTPUT_DIR  ../outputs/cards-v4/fast_rcnn_R_50_FPN_3x/ \
     SOLVER.IMS_PER_BATCH 2 
--- notebooks/layout-workflow.py
+++ notebooks/layout-workflow.py
@@ -7,19 +7,21 @@
 import layoutparser as lp
 import cv2
 import pandas as pd
-# import json
 
-## LOAD IMAGES
+## 1 - LOAD IMAGES
 print("Loading image files...")
-image_files = "/nfs/turbo/isr-jtalexnonsen/sample_scans/274-01-0013.jpg"
+image_files = "/nfs/turbo/isr-jtalexnonsen/images/783095-02/783095-02-0053.jpg" # random, unseen card
 image = cv2.imread(image_files)
 
-## DETECT LAYOUT WITH CUSTOM MODEL
+## 2 - DETECT LAYOUT WITH CUSTOM MODEL
 print("Detecting layout with custom model...")
-# model = lp.Detectron2LayoutModel('lp://PrimaLayout/mask_rcnn_R_50_FPN_3x/config') # pretrained model
-model = lp.Detectron2LayoutModel(config_path = "/nfs/turbo/isr-jtalexnonsen/models/V2_fast_rcnn_R_50_FPN_3x/config.yaml", model_path = "/nfs/turbo/isr-jtalexnonsen/models/V2_fast_rcnn_R_50_FPN_3x/model_0039999.pth", extra_config=["MODEL.ROI_HEADS.SCORE_THRESH_TEST", 0.8])
+model = lp.Detectron2LayoutModel(config_path = "../layout-model-training/outputs/cards-v4/fast_rcnn_R_50_FPN_3x/config.yaml", model_path = "../layout-model-training/outputs/cards-v4/fast_rcnn_R_50_FPN_3x/model_final.pth", extra_config=["MODEL.ROI_HEADS.SCORE_THRESH_TEST", 0.8], label_map={0: "AGENCY", 1: "AMOUNT", 2: "ID", 3:"LOCATION", 4:"NAME", 5:"OTHER", 6:"STATUS"})
+
+# model = lp.Detectron2LayoutModel(config_path = "/nfs/turbo/isr-jtalexnonsen/models/V2_fast_rcnn_R_50_FPN_3x/config.yaml", model_path = "/nfs/turbo/isr-jtalexnonsen/models/V2_fast_rcnn_R_50_FPN_3x/model_0039999.pth", extra_config=["MODEL.ROI_HEADS.SCORE_THRESH_TEST", 0.8])
+
 layout_result = model.detect(image)
 # print(layout_result)
+
 text_blocks = lp.Layout([b for b in layout_result])
 image_width = len(image[0])
 left_interval = lp.Interval(0, image_width/2, axis='x').put_on_canvas(image)
@@ -29,9 +31,8 @@ right_blocks = [b for b in text_blocks if b not in left_blocks]
 right_blocks.sort(key = lambda b:b.coordinates[1])
 text_blocks = lp.Layout([b.set(id = idx) for idx, b in enumerate(left_blocks + right_blocks)])
 print(f"{len(text_blocks)} text blocks detected.")
-# print(f"Text blocks are {type(text_blocks)}.") # text_blocks is class 'layoutparser.elements.layout.Layout'
 
-## EXTRACT TEXT FROM BOUNDING BOXES
+## 3 - EXTRACT TEXT FROM BOUNDING BOXES
 ocr_agent = lp.TesseractAgent(languages='eng')
 print("Extracting text...")
 for block in text_blocks:
@@ -47,12 +48,12 @@ for txt in text_blocks.get_texts():
     print(txt, end='\n---\n') # txt is layoutparser.elements.layout_elements.TextBlock
     extract_list.append(txt)
 
-print(extract_list)
+# print(extract_list)
 
-## STRUCTURE AND SAVE TEXT
+## 4 - STRUCTURE AND SAVE TEXT
 
-df = pd.DataFrame(extract_list, columns=['A', 'B', 'C', 'D', 'E', 'F']) # solution will need to be a list of lists or dictionary
-df.to_csv('/nfs/turbo/isr-jtalexnonsen/extracts/test.csv', index=None)
+# df = pd.DataFrame(extract_list, columns=['A', 'B', 'C', 'D', 'E', 'F']) # solution will need to be a list of lists or dictionary
+# df.to_csv('/nfs/turbo/isr-jtalexnonsen/extracts/test.csv', index=None)
 
 # save_blocks = json.dumps(extract_list)
 # file_path = "/nfs/turbo/isr-jtalexnonsen/extracts"
@@ -60,4 +61,4 @@ df.to_csv('/nfs/turbo/isr-jtalexnonsen/extracts/test.csv', index=None)
 # blocks_file.write(save_blocks)
 # blocks_file.close()
 
-print(f"Extraction completed. File saved to {file_path}.")
+# print(f"Extraction completed. File saved to {file_path}.")
