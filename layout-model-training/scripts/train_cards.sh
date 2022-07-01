9c252e758d944ba296baa77db04a5c46af4022c3
Sara Katherine Lafia <slafia@gl-login1.arc-ts.umich.edu>
2022-07-01 09:36:45 -0400

    Scripts to extract text from cards

--- layout-model-training/scripts/train_cards.sh
+++ layout-model-training/scripts/train_cards.sh
@@ -4,10 +4,10 @@ cd /home/slafia/layout-parser/layout-model-training/tools
 
 python train_net.py \
     --dataset_name          card-item \
-    --json_annotation_train ../data/cards-v4/train.json \
-    --image_path_train      ../data/cards-v4/ \
-    --json_annotation_val   ../data/cards-v4/test.json \
-    --image_path_val        ../data/cards-v4/ \
+    --json_annotation_train ../data/cards-v5/train.json \
+    --image_path_train      ../data/cards-v5/ \
+    --json_annotation_val   ../data/cards-v5/test.json \
+    --image_path_val        ../data/cards-v5/ \
     --config-file           ../configs/prima/fast_rcnn_R_50_FPN_3x.yaml \
-    OUTPUT_DIR  ../outputs/cards-v4/fast_rcnn_R_50_FPN_3x/ \
+    OUTPUT_DIR  ../outputs/cards-v5/fast_rcnn_R_50_FPN_3x/ \
     SOLVER.IMS_PER_BATCH 2 
--- notebooks/layout-workflow.py
+++ notebooks/layout-workflow.py
@@ -7,58 +7,90 @@
 import layoutparser as lp
 import cv2
 import pandas as pd
+import glob
+import os
 
-## 1 - LOAD IMAGES
-print("Loading image files...")
-image_files = "/nfs/turbo/isr-jtalexnonsen/images/783095-02/783095-02-0053.jpg" # random, unseen card
-image = cv2.imread(image_files)
-
-## 2 - DETECT LAYOUT WITH CUSTOM MODEL
-print("Detecting layout with custom model...")
-model = lp.Detectron2LayoutModel(config_path = "../layout-model-training/outputs/cards-v4/fast_rcnn_R_50_FPN_3x/config.yaml", model_path = "../layout-model-training/outputs/cards-v4/fast_rcnn_R_50_FPN_3x/model_final.pth", extra_config=["MODEL.ROI_HEADS.SCORE_THRESH_TEST", 0.8], label_map={0: "AGENCY", 1: "AMOUNT", 2: "ID", 3:"LOCATION", 4:"NAME", 5:"OTHER", 6:"STATUS"})
-
-# model = lp.Detectron2LayoutModel(config_path = "/nfs/turbo/isr-jtalexnonsen/models/V2_fast_rcnn_R_50_FPN_3x/config.yaml", model_path = "/nfs/turbo/isr-jtalexnonsen/models/V2_fast_rcnn_R_50_FPN_3x/model_0039999.pth", extra_config=["MODEL.ROI_HEADS.SCORE_THRESH_TEST", 0.8])
-
-layout_result = model.detect(image)
-# print(layout_result)
-
-text_blocks = lp.Layout([b for b in layout_result])
-image_width = len(image[0])
-left_interval = lp.Interval(0, image_width/2, axis='x').put_on_canvas(image)
-left_blocks = text_blocks.filter_by(left_interval, center=True)._blocks
-left_blocks.sort(key = lambda b:b.coordinates[1])
-right_blocks = [b for b in text_blocks if b not in left_blocks]
-right_blocks.sort(key = lambda b:b.coordinates[1])
-text_blocks = lp.Layout([b.set(id = idx) for idx, b in enumerate(left_blocks + right_blocks)])
-print(f"{len(text_blocks)} text blocks detected.")
-
-## 3 - EXTRACT TEXT FROM BOUNDING BOXES
+# image_files = sorted(glob.glob('/nfs/turbo/isr-jtalexnonsen/sample_scans/'+'/*.jpg'))
+# image_files = sorted(glob.glob('/nfs/turbo/isr-jtalexnonsen/images/783095-23/'+'/*.jpg'))
+image_files = sorted(glob.glob('/nfs/turbo/isr-jtalexnonsen/flat_images/'+'/*.jpg'))
+print(f"Loading {len(image_files)} image files...")
 ocr_agent = lp.TesseractAgent(languages='eng')
-print("Extracting text...")
-for block in text_blocks:
-    segment_image = (block
+print("Initializing OCR engine...")
+
+df_list = []
+
+for file in image_files:
+    file_name = os.path.basename(file)
+    image = cv2.imread(file)
+    print(f"Detecting layout for {file_name}...")
+    model = lp.Detectron2LayoutModel(config_path = "../layout-model-training/outputs/cards-v4/fast_rcnn_R_50_FPN_3x/config.yaml", model_path = "../layout-model-training/outputs/cards-v4/fast_rcnn_R_50_FPN_3x/model_final.pth", extra_config=["MODEL.ROI_HEADS.SCORE_THRESH_TEST", 0.8], label_map={0: "AGENCY", 1: "AMOUNT", 2: "ID", 3:"LOCATION", 4:"NAME", 5:"OTHER", 6:"STATUS"})
+    layout_result = model.detect(image)
+    text_blocks = lp.Layout([b for b in layout_result])
+    image_width = len(image[0])
+    left_interval = lp.Interval(0, image_width/2, axis='x').put_on_canvas(image)
+    left_blocks = text_blocks.filter_by(left_interval, center=True)._blocks
+    left_blocks.sort(key = lambda b:b.coordinates[1])
+    right_blocks = [b for b in text_blocks if b not in left_blocks]
+    right_blocks.sort(key = lambda b:b.coordinates[1])
+    text_blocks = lp.Layout([b.set(id = idx) for idx, b in enumerate(left_blocks + right_blocks)])
+    print(f"{len(text_blocks)} text blocks detected.")
+    for block in text_blocks:
+        segment_image = (block
                         .pad(left=15, right=15, top=5, bottom=5)
                         .crop_image(image))
-    text = ocr_agent.detect(segment_image)
-    block.set(text=text, inplace=True)
+        text = ocr_agent.detect(segment_image)
+        block.set(text=text, inplace=True)
+    df = text_blocks.to_dataframe()
+    df['file_name'] = file_name
+    df_list.append(df)
+    print(f"Text extracted for {file_name}...")
 
-extract_list = []
+df_output = pd.concat(df_list, axis=0, ignore_index=True)
+print(f"{len(df_list)} dataframes concatenated...")
 
-for txt in text_blocks.get_texts():
-    print(txt, end='\n---\n') # txt is layoutparser.elements.layout_elements.TextBlock
-    extract_list.append(txt)
+df_pivot = df_output.pivot_table(index=['file_name'],
+                                     columns='type', 
+                                     values='text',
+                                     aggfunc=lambda x: ' '.join(x))
 
-# print(extract_list)
+out_path = '/nfs/turbo/isr-jtalexnonsen/extracts/'
+df_pivot.to_csv(out_path+'flat_images.csv')
+print(f"Extraction completed. File saved to {out_path}.")
+
+## 1 - LOAD IMAGES
+# file_name = "783095-02-0053.jpg"
+# image_files = image_dir+file_name # random, unseen card
+# image = cv2.imread(image_files)
+
+## 2 - DETECT LAYOUT WITH CUSTOM MODEL
+# print("Detecting layout with custom model...")
+# model = lp.Detectron2LayoutModel(config_path = "../layout-model-training/outputs/cards-v5/fast_rcnn_R_50_FPN_3x/config.yaml", model_path = "../layout-model-training/outputs/cards-v5/fast_rcnn_R_50_FPN_3x/model_final.pth", extra_config=["MODEL.ROI_HEADS.SCORE_THRESH_TEST", 0.8], label_map={0: "AGENCY", 1: "AMOUNT", 2: "ID", 3:"LOCATION", 4:"NAME", 5:"OTHER", 6:"STATUS"})
+# layout_result = model.detect(image)
+# text_blocks = lp.Layout([b for b in layout_result])
+# image_width = len(image[0])
+# left_interval = lp.Interval(0, image_width/2, axis='x').put_on_canvas(image)
+# left_blocks = text_blocks.filter_by(left_interval, center=True)._blocks
+# left_blocks.sort(key = lambda b:b.coordinates[1])
+# right_blocks = [b for b in text_blocks if b not in left_blocks]
+# right_blocks.sort(key = lambda b:b.coordinates[1])
+# text_blocks = lp.Layout([b.set(id = idx) for idx, b in enumerate(left_blocks + right_blocks)])
+# print(f"{len(text_blocks)} text blocks detected.")
+
+## 3 - EXTRACT TEXT FROM BOUNDING BOXES
+# ocr_agent = lp.TesseractAgent(languages='eng')
+# print("Extracting text...")
+# for block in text_blocks:
+#     segment_image = (block
+#                         .pad(left=15, right=15, top=5, bottom=5)
+#                         .crop_image(image))
+#     text = ocr_agent.detect(segment_image)
+#     block.set(text=text, inplace=True)
 
 ## 4 - STRUCTURE AND SAVE TEXT
-
-# df = pd.DataFrame(extract_list, columns=['A', 'B', 'C', 'D', 'E', 'F']) # solution will need to be a list of lists or dictionary
-# df.to_csv('/nfs/turbo/isr-jtalexnonsen/extracts/test.csv', index=None)
-
-# save_blocks = json.dumps(extract_list)
+# print("Saving text...")
 # file_path = "/nfs/turbo/isr-jtalexnonsen/extracts"
-# blocks_file = open(file_path+"/test.json", "w")
-# blocks_file.write(save_blocks)
-# blocks_file.close()
-
+# df = text_blocks.to_dataframe()
+# df['file_name'] = file_name
+# df_pivot = df.set_index(['file_name'], drop=True).unstack("type")
+# df_pivot.to_csv('/nfs/turbo/isr-jtalexnonsen/extracts/test.csv', index=None)
 # print(f"Extraction completed. File saved to {file_path}.")
